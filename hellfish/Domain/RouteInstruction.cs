3a3c8e2238d72a0cc5090a6401997fe9c8d4f798
cristiangiagante <cristiangiagante@hotmail.com>
2023-09-19 20:13:28 -0300

    Initial commit

---
+++ hellfish/Domain/RouteInstruction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace hellfish.Domain
+{
+	public class RouteInstruction
+	{
+		public RouteInstruction()
+		{
+		}
+
+		public int Stage { get; set; }
+
+		public string? Description { get; set; }
+	}
+}
+
---
+++ hellfish/Domain/Trail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace hellfish.Domain
+{
+	public class Trail
+	{
+		public Trail()
+		{
+		}
+
+		public int Id { get; set; }
+
+		public string? Name { get; set; }
+
+		public string? Description { get; set; }
+
+		public string? Image { get; set; }
+
+		public string? Location { get; set; }
+
+		public int TimeInMinutes { get; set; }
+
+		public string TimeFormated => $"{TimeInMinutes/60}h {TimeInMinutes%60}m";
+
+		public int Length { get; set; }
+
+		public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+	}
+}
+
