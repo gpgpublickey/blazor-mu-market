c649a8b457d416e6b2ff85eb653f4d89e503769f
Cristian Giagante <cristiangiagante@hotmail.com>
2024-06-09 12:41:11 -0300

    Update nugets 4

--- hellfish/Features/Home/Mapper.cs
+++ hellfish/Features/Home/Mapper.cs
@@ -9,6 +9,12 @@ namespace hellfish.Features.Home
         public Mapper()
         {
             CreateMap<SellResponse, Sell>()
+                .ForMember(dst => dst.Id, opt => opt.Ignore())
+                .ForMember(dst => dst.Length, opt => opt.Ignore())
+                .ForMember(dst => dst.Location, opt => opt.Ignore())
+                .ForMember(dst => dst.TimeFormated, opt => opt.Ignore())
+                .ForMember(dst => dst.TimeInMinutes, opt => opt.Ignore())
+                .ForMember(dst => dst.Karma, opt => opt.MapFrom(x => x.Karma))
                 .ForMember(dst => dst.Name, opt => opt.MapFrom(x => x.Author))
                 .ForMember(dst => dst.Image, opt => opt.MapFrom(x => x.Img))
                 .ForMember(dst => dst.TimeInMinutes, opt => opt.MapFrom(x => (DateTime.UtcNow - x.CreatedAt).TotalMinutes))
