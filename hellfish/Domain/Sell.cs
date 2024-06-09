918157efbf73070bcde700f268abe73c1864eefd
Cristian Giagante <cristiangiagante@hotmail.com>
2024-06-09 11:03:09 -0300

    More updes for deployment

--- hellfish/Domain/Sell.cs
+++ hellfish/Domain/Sell.cs
@@ -3,10 +3,6 @@ namespace hellfish.Domain
 {
 	public class Sell
 	{
-		public Sell()
-		{
-		}
-
 		public int Id { get; set; }
 
 		public string? Name { get; set; }
@@ -22,8 +18,6 @@ namespace hellfish.Domain
 		public string TimeFormated => $"{TimeInMinutes/60}h {TimeInMinutes%60}m";
 
 		public int Length { get; set; }
-
-		public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
 
         public string PhoneLink { get; set; }
 
